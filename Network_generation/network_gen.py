65820cead098828c8e50a172fecaab0282dd493b
A.Sudarshan <f20190744@pilani.bits-pilani.ac.in>
2024-10-20 00:15:08 -0400

    fixed the degree distribution function

--- Network_generation/network_gen.py
+++ Network_generation/network_gen.py
@@ -16,12 +16,46 @@ def save_edgelist(G: nx.Graph, save_file: str)-> None:
         G (nx.Graph): Graph to save
         save_file (str): File name for the edgelist file is saved.
     """
-    str_edges = [f"{e[0]} {e[1]}" for e in list(G.edges)]
+    str_edges = [f"{e[0]} {e[1]}\n" for e in list(G.edges)]
 
     save_path = "../data/networks/" + save_file
 
     with open(save_path, "w") as f:
         f.writelines(str_edges)
+
+# Havel-Hakimi algorithm
+def havel_hakimi(deg_seq: list) -> nx.Graph:
+    """Generates a graph from a given degree sequence using the Havel-Hakimi algorithm.
+
+    Args:
+        deg_seq (list): Degree sequence of the graph
+
+    Returns:
+        nx.Graph: Graph generated from the degree sequence
+    """
+    G = nx.Graph()
+    G.add_nodes_from(range(len(deg_seq)))
+    deg_seq = sorted(deg_seq, reverse=True)
+    deg_seq = [list(x) for x in zip(range(len(deg_seq)), deg_seq)]
+    edges = []
+
+    while len(deg_seq) > 0:
+        # Pick the vertex with lowest degree
+        v = deg_seq.pop()
+        # If the degree is greater than the number of remaining vertices, return None
+        if v[1] > len(deg_seq):
+            return None
+        # Connect the vertex to the v vertices having the highest remaining degree
+        for i in range(v[1]):
+            deg_seq[i][1] -= 1
+            edges.append((v[0], deg_seq[i][0]))
+        # Remove the vertex from the list if its degree is 0
+        deg_seq = [d for d in deg_seq if d[1] != 0]
+        deg_seq = sorted(deg_seq, key=lambda x: x[1], reverse=True)
+    
+    G.add_edges_from(edges)
+    
+    return G
 
 # Homogeneous network
 def gen_homog(n: int, k: Optional[int] = None) -> nx.Graph:
@@ -50,20 +84,43 @@ def gen_hetero_deg(n:int, mean: float, var: float) -> nx.Graph:
     average degree and variance. The degrees are sampled from 
 
     Args:
-        n (int): _description_
-        mean (float): _description_
-        var (float): _description_
+        n (int): Number of nodes in the network
+        mean (float): Average degree of the network
+        var (float): Variance of the degree distribution
 
     Returns:
-        nx.Graph: _description_
+        nx.Graph: Final network with the required degree distribution
     """
-    scale_param = var/(mean-1) # k of gamma dist
-    shape_param = (mean-1)/scale_param # theta of gamma dist
+    s = []
+    theta = var[0] / (mean - 1)
+    k = (mean - 1) / theta
+    samples = np.random.gamma(k - 1, theta, len(var) * n)
+    
+    for i in range(len(var)):
+        s.append([int(x) + 1 for x in samples[i*n:(i+1)*n]])
+    
+    act_means = [np.mean(sample) for sample in s]
+    
+    for i in range(1, len(var)):
+        for j in range(len(s[i]) - 1):
+            p = np.random.random() * var[i]
+            s[i][j] += (act_means[0] - act_means[i]) - p
+            s[i][j + 1] += (act_means[0] - act_means[i]) + p
+        
+        s[i] = [int(x)+1 for x in s[i]]
+    
+    print(f"Mean={[np.mean([int(x) + 1 for x in sample]) for sample in s]} Variance={[np.var([int(x) + 1 for x in sample]) for sample in s]}")
 
-    samples = np.random.gamma(scale_param - 1,shape_param,n)
-    deg = [int(x) + 1 for x in samples]
+    # Generate graph from degree sequence
+    G = []
+    for i in range(len(var)):
+        g = havel_hakimi(s[i])
+        G.append(g)
+        deg1 = [g.degree(n) for n in g.nodes]
+        print(f"Mean: {np.mean(deg1)}, Variance: {np.var(deg1)}")
+        print(f"Number of connected components: {nx.number_connected_components(g)}")
 
-    return nx.random_degree_sequence_graph(deg)
+    return G
 
 def gen_small_world(n:int, k:int, p:float) -> nx.Graph:
     """Generates a small world network on n nodes with average degree k and rewiring probability p.
@@ -79,7 +136,7 @@ def gen_small_world(n:int, k:int, p:float) -> nx.Graph:
     return nx.watts_strogatz_graph(n,k,p)
 
 # Heterogeneous network - clustering coeffecient
-def gen_hetero_cc(n:int, k: float, cc:float, tol:float=1e-3) -> nx.Graph:
+def gen_hetero_cc(n:int, k: float, cc:float, p:float=0.1, tol:float=1e-3) -> nx.Graph:
     """Generates a network with target clustering coeffecient.
 
     Args:
@@ -92,7 +149,7 @@ def gen_hetero_cc(n:int, k: float, cc:float, tol:float=1e-3) -> nx.Graph:
     """
 
     # Take a small world network of same size and average degree
-    G = gen_small_world(n,math.floor(k),0.2)
+    G = gen_small_world(n,math.floor(k),p)
 
     gcc = nx.transitivity(G) # Global clustering coeffecient
     nodes = list(G.nodes)
@@ -139,19 +196,13 @@ n = 1000
 # save_edgelist(B,"homog_regular.txt")
 # print("Generated regular graph")
 
-# # Generate hetero graph - degree distribution
-# C = gen_hetero_deg(n,4,1)
-# save_edgelist(C,"hetero_deg_var_1.txt")
-# print("Generated for mu=4 and var=1")
+# Generate hetero graph - degree distribution
+C, D, E = gen_hetero_deg(n,mean=4,var=[1,3,6])
+save_edgelist(C,"hetero_deg_var_1.txt")
+save_edgelist(D,"hetero_deg_var_3.txt")
+save_edgelist(E,"hetero_deg_var_6.txt")
 
-# D = gen_hetero_deg(n,4,3)
-# save_edgelist(D,"hetero_deg_var_3.txt")
-# print("Generated for mu=4 and var=3")
-
-# E = gen_hetero_deg(n,4,6)
-# save_edgelist(E,"hetero_deg_var_6.txt")
-# print("Generated for mu=4 and var=6")
-
-# Generate hetero graph - clustering coeffecient
-F = gen_hetero_cc(n,4,0.24)
-save_edgelist(F,"hetero_cc_0.24.txt")
+# # Generate hetero graph - clustering coeffecient
+# F = gen_hetero_cc(n,k=4,cc=0.24,p=0.1)
+# save_edgelist(F,"hetero_cc_0.24.txt")
+# Generate 3 samples from gamma distribution each of size n and each with same mean equal to 4 but different variances 1, 3, and 6 respectively
