ca36d5b40e8b313a54e91064e3befef415489dec
A.Sudarshan <f20190744@pilani.bits-pilani.ac.in>
2024-10-24 22:47:49 -0400

    fixed the network generation

--- Network_generation/network_gen.py
+++ Network_generation/network_gen.py
@@ -8,6 +8,8 @@ import numpy as np
 import math
 from typing import Optional, Literal
 import os
+import random
+from scipy.stats import gamma
 
 def save_edgelist(G: nx.Graph, save_file: str)-> None:
     """Saves the edgelist of the graph G.
@@ -16,15 +18,37 @@ def save_edgelist(G: nx.Graph, save_file: str)-> None:
         G (nx.Graph): Graph to save
         save_file (str): File name for the edgelist file is saved.
     """
-    str_edges = [f"{e[0]} {e[1]}\n" for e in list(G.edges)]
+    edges = list(G.edges)
+    str_edges = [f"{e[0]} {e[1]}\n" for e in edges]
 
     save_path = "../data/networks/" + save_file
 
     with open(save_path, "w") as f:
         f.writelines(str_edges)
+# Handle isolated nodes
+def handle_isolated_nodes(G: nx.Graph, iso: list, mean:int) -> nx.Graph:
+
+    comps = sorted(list(nx.connected_components(G)),key=len,reverse=True)
+    comp = comps[0]
+    nodes = list(comp)
+    
+    G1 = nx.random_regular_graph(mean, len(iso))
+    x = sorted(nodes, key=lambda x: G.degree(x), reverse=True)
+    x = x[0]
+    y = random.choice(list(G1.nodes))
+    G.add_edge(x,y)
+
+    for e in list(G1.edges):
+        G.add_edge(iso[e[0]],iso[e[1]])
+
+
+    print(f"Number of isolated nodes: {len([v for v in G.nodes if G.degree(v) == 0])}")
+    print(f"Number of connected components: {nx.number_connected_components(G)}")
+    
+    return G
 
 # Havel-Hakimi algorithm
-def havel_hakimi(deg_seq: list) -> nx.Graph:
+def havel_hakimi(deg_seq: list, mean:int) -> nx.Graph:
     """Generates a graph from a given degree sequence using the Havel-Hakimi algorithm.
 
     Args:
@@ -34,26 +58,34 @@ def havel_hakimi(deg_seq: list) -> nx.Graph:
         nx.Graph: Graph generated from the degree sequence
     """
     G = nx.Graph()
-    G.add_nodes_from(range(len(deg_seq)))
-    deg_seq = sorted(deg_seq, reverse=True)
-    deg_seq = [list(x) for x in zip(range(len(deg_seq)), deg_seq)]
+    deg_seq1 = deg_seq.copy()
+    G.add_nodes_from(range(len(deg_seq1)))
+    deg_seq1 = sorted(deg_seq1, reverse=True)
+    p = deg_seq1.copy()
+    deg_seq1 = [list(x) for x in zip(range(len(deg_seq1)), deg_seq1)]
     edges = []
 
-    while len(deg_seq) > 0:
+    while len(deg_seq1) > 0:
         # Pick the vertex with lowest degree
-        v = deg_seq.pop()
+        v = deg_seq1.pop()
         # If the degree is greater than the number of remaining vertices, return None
-        if v[1] > len(deg_seq):
+        if v[1] > len(deg_seq1):
             return None
         # Connect the vertex to the v vertices having the highest remaining degree
         for i in range(v[1]):
-            deg_seq[i][1] -= 1
-            edges.append((v[0], deg_seq[i][0]))
+            deg_seq1[i][1] -= 1
+            edges.append((v[0], deg_seq1[i][0]))
         # Remove the vertex from the list if its degree is 0
-        deg_seq = [d for d in deg_seq if d[1] != 0]
-        deg_seq = sorted(deg_seq, key=lambda x: x[1], reverse=True)
+        deg_seq1 = [d for d in deg_seq1 if d[1] != 0]
+        deg_seq1 = sorted(deg_seq1, key=lambda x: x[1], reverse=True)
     
     G.add_edges_from(edges)
+    iso = [(v,p[v]) for v in G.nodes if G.degree(v) == 0]
+    
+    # Connect isolated nodes to the node with the highest degree
+    if iso:
+        print(f"Number of isolated nodes: {len(iso)}")
+        G = handle_isolated_nodes(G, iso, mean)
     
     return G
 
@@ -79,42 +111,62 @@ def gen_homog(n: int, k: Optional[int] = None) -> nx.Graph:
         return nx.random_regular_graph(k,n)
  
 # Heterogeneous network - degree distribution
-def gen_hetero_deg(n:int, mean: float, var: float) -> nx.Graph:
+def gen_hetero_deg(n:int, mean: float, var: list[int]) -> nx.Graph:
     """Generates a network with skewed degree distribution dictated by an
     average degree and variance. The degrees are sampled from 
 
     Args:
         n (int): Number of nodes in the network
         mean (float): Average degree of the network
-        var (float): Variance of the degree distribution
+        var (list[int]): Variance of the degree distribution
 
     Returns:
         nx.Graph: Final network with the required degree distribution
     """
+    np.random.seed(11)
     s = []
-    theta = var[0] / (mean - 1)
-    k = (mean - 1) / theta
-    samples = np.random.gamma(k - 1, theta, len(var) * n)
+
+    # # Get parameters for gamma distribution
+    # theta = var[0]/ (mean - 1)
+    # k = (mean - 1)**2 / var[0]
+
+    # # Generate samples from gamma distribution
+    # samples = gamma.rvs(a=theta, scale=k, size=len(var)*n)
     
-    for i in range(len(var)):
-        s.append([int(x) + 1 for x in samples[i*n:(i+1)*n]])
+    # # Split and discretize the sample
+    # for i in range(len(var)):
+    #     s.append([round(x) + 1 for x in samples[i*n:(i+1)*n] if (x - int(x)) > 0])
     
-    act_means = [np.mean(sample) for sample in s]
-    
-    for i in range(1, len(var)):
-        for j in range(len(s[i]) - 1):
-            p = np.random.random() * var[i]
-            s[i][j] += (act_means[0] - act_means[i]) - p
-            s[i][j + 1] += (act_means[0] - act_means[i]) + p
+    # act_means = [np.mean(sample) for sample in s]
+    # for i in range(1, len(var)):
+    #     for j in range(len(s[i]) - 1):
+    #         p = np.random.random() * var[i]
+    #         s[i][j] += (act_means[0] - act_means[i]) - p
+    #         s[i][j + 1] += (act_means[0] - act_means[i]) + p
         
-        s[i] = [int(x)+1 for x in s[i]]
+    #     s[i] = [round(x) + 1 for x in s[i] if (x - int(x))!= 0.0]
+
+    # # Check if all lists in s contain integers and print
+    # for idx, sample in enumerate(s):
+    #     if all(isinstance(x, int) for x in sample):
+    #         print(f"List {idx} contains all integers.")
+    #     else:
+    #         print(f"List {idx} does not contain all integers.")
+
+
+    # Generate samples from gamma distribution
+    for i in range(len(var)):
+        s.append([round(x) + 1 for x in gamma.rvs(a=var[i]/(mean-1), scale=(mean-1)**2/var[i], size=n, random_state=42) if (x - int(x)) > 0])
     
-    print(f"Mean={[np.mean([int(x) + 1 for x in sample]) for sample in s]} Variance={[np.var([int(x) + 1 for x in sample]) for sample in s]}")
+    
+    print(f"Mean={[np.mean(sample) for sample in s]} Variance={[np.var(sample) for sample in s]}")
 
     # Generate graph from degree sequence
     G = []
     for i in range(len(var)):
-        g = havel_hakimi(s[i])
+        print(f"*********************************** FOR var={var[i]} ***********************************")
+        g = havel_hakimi(s[i],mean)
+        # g = nx.havel_hakimi_graph(s[i])
         G.append(g)
         deg1 = [g.degree(n) for n in g.nodes]
         print(f"Mean: {np.mean(deg1)}, Variance: {np.var(deg1)}")
@@ -197,11 +249,9 @@ n = 1000
 # print("Generated regular graph")
 
 # Generate hetero graph - degree distribution
-C, D, E = gen_hetero_deg(n,mean=4,var=[1,3,6])
-save_edgelist(C,"hetero_deg_var_1.txt")
-save_edgelist(D,"hetero_deg_var_3.txt")
-save_edgelist(E,"hetero_deg_var_6.txt")
-
+G = gen_hetero_deg(n,mean=4,var=[1,3,6])
+for i in range(len(G)):
+    save_edgelist(G[i],f"hetero_deg_var_{round(np.var([G[i].degree(x) for x in list(G[i].nodes)]),2)}.txt")
 # # Generate hetero graph - clustering coeffecient
 # F = gen_hetero_cc(n,k=4,cc=0.24,p=0.1)
 # save_edgelist(F,"hetero_cc_0.24.txt")
